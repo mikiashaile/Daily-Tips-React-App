6a1b496fff1a62614764f70bc5aae77c065889e9
Mikias <113710566+mikiashaile@users.noreply.github.com>
2023-09-18 18:48:37 +0000

    lifting up state to handle sibling comp communication

--- src/Accordion.js
+++ src/Accordion.js
@@ -2,18 +2,27 @@ import { useState } from "react";
 import "./accordion.css";
 
 export default function Accordion({ data }) {
+    const [currentOpen, setCurrentOpen] = useState(null);
+
   return (
   <div className="accordion">
     {data.map((item, index) => 
-    <AccordionItem num={index} title={item.title} text={item.text} key={item.title}/>
+    <AccordionItem 
+        num={index} 
+        title={item.title} 
+        text={item.text} 
+        key={item.title} currentOpen={currentOpen} onToggle={setCurrentOpen}/>
     )}
   </div>);
 }
 
-function AccordionItem({num, title, text}) {
-    const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({num, title, text, currentOpen, onToggle }) {
+    const isOpen = num === currentOpen;
+    function handleToggle() {
+        onToggle(num)
+    }
     return (
-        <div className={`item ${isOpen ? 'open' : ''}`} onClick={() => setIsOpen(!isOpen)} >
+        <div className={`item ${isOpen ? 'open' : ''}`} onClick={handleToggle} >
             <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
             <p className="title">{title}</p>
             <p className="icon">{isOpen ? '-' : '+'}</p>
